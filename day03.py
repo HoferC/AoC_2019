f0387c4c8134fa9c734d72ceedb77a78414f0de8
Chris Hofer <chris@chrishofer.com>
2019-12-04 22:18:43 -0500

    Day3B done

--- day03.py
+++ day03.py
@@ -58,6 +58,13 @@ class Wire:
                             # print(c1, '\t', c1.manhattan())
         return intersections
 
+    def distanceToCoord(self, coord):
+        dist = 0
+        for c in self.coords:
+            if c == coord:
+                return dist
+            dist += 1
+
 
 class Coordinate:
     x = 0
@@ -70,8 +77,12 @@ class Coordinate:
     def __str__(self):
         return str(self.x) + ",\t" + str(self.y)
 
+    def __eq__(self, other):
+        return isinstance(other, Coordinate) and other.x == self.x and other.y == self.y
+
     def manhattan(self):
         return abs(self.x) + abs(self.y)
+
 
 def star1():
     print('Star 1')
@@ -95,3 +106,18 @@ def star1():
 
 def star2():
     print('Star 2')
+    w1 = Wire()
+    w2 = Wire()
+    with open("puzzles/day3.txt", "r") as f:
+        instructionString = f.readline()
+        instructions = instructionString.split(',')
+        w1.parse(instructions)
+        instructionString = f.readline()
+        instructions = instructionString.split(',')
+        w2.parse(instructions)
+
+        # Get the intersection points (list of Coordinate)
+        intPoints = w1.findIntersections(w2)
+        print("********")
+        print(min((w1.distanceToCoord(wi) + w2.distanceToCoord(wi)) for wi in intPoints))
+        print("********")
